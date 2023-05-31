0595cbc6bd22a620e9b994a399246953b8b17df3
yasirkhana <khan100555yasir@gmail.com>
2023-05-31 22:24:26 +0500

    Assignment 4 done
    assignment 3 done

--- baham/views.py
+++ baham/views.py
@@ -261,3 +261,149 @@ def delete_vehicle_model(request, uuid):
         return JsonResponse(response_data, status=200)
     else:
         return JsonResponse({'error': 'Invalid endpoint or method type'}, status=400)
+
+
+
+        def get_all_user_profiles(request):
+    if request.method == 'GET':
+        users = UserProfile.objects.filter(voided=False)
+        if users:
+            data = []
+            for user in users:
+                data.append({
+                    'uuid': str(user.uuid),
+                    'birthdate': user.birthdate,
+                    'gender': user.gender,
+                    'type': user.type,
+                    'primary_contact': user.primary_contact,
+                    'address': user.address,
+                    'town': user.town,
+                    'bio': user.bio,
+                    'date_created': user.date_created,
+                    'created_by': str(user.created_by),
+                })
+            return JsonResponse({"results": data}, status=200)
+        else:
+            return JsonResponse({'error': 'User Profiles Not Found'}, status=404)
+    else:
+        return JsonResponse({'error': 'Invalid endpoint or method type'}, status=400)
+
+
+def get_user_profile(request, uuid):
+    if request.method == 'GET':
+        user_model = UserProfile.objects.filter(uuid=uuid, voided=False).first()
+        if user_model:
+            data = {
+                'uuid': str(user_model.uuid),
+                'birthdate': user_model.birthdate,
+                'gender': user_model.gender,
+                'type': user_model.type,
+                'primary_contact': user_model.primary_contact,
+                'address': user_model.address,
+                'town': user_model.town,
+                'bio': user_model.bio,
+                'date_created': user_model.date_created,
+                'created_by': str(user_model.created_by),
+            }
+            return JsonResponse({'results': data}, status=200)
+        else:
+            return JsonResponse({'error': 'User Profile Not Found'}, status=404)
+    else:
+        return JsonResponse({'error': 'Invalid endpoint or method type'}, status=400)
+
+
+def create_user_profile(request):
+    if request.method == 'POST':
+        _username = request.POST.get('username')
+        _pass = request.POST.get('pass')
+        _birthdate = request.POST.get('birthdate')
+        _gender = request.POST.get('gender')
+        _type = request.POST.get('type')
+        _primary_contact = request.POST.get('primary_contact')
+        _alternate_contact = request.POST.get('alternate_contact')
+        _address = request.POST.get('address')
+        _address_latitude = request.POST.get('address_latitude')
+        _address_longitude = request.POST.get('address_longitude')
+        _landmark = request.POST.get('landmark')
+        _town = request.POST.get('town')
+        _active = request.POST.get('active')
+        _bio = request.POST.get('bio')
+
+        user_profile = UserProfile.objects.create(
+            user=User.objects.create_user(username=_username, password=_pass),
+            birthdate=_birthdate,
+            gender=_gender,
+            type=_type,
+            primary_contact=_primary_contact,
+            alternate_contact=_alternate_contact,
+            address=_address,
+            address_latitude=_address_latitude,
+            address_longitude=_address_longitude,
+            landmark=_landmark,
+            town=_town,
+            active=_active,
+            bio=_bio,
+        )
+
+        response_data = {
+            'message': 'User profile created successfully',
+            'uuid': str(user_profile.uuid),
+        }
+        return JsonResponse(response_data, status=201)
+    else:
+        return JsonResponse({'error': 'Invalid endpoint or method type'}, status=400)
+
+
+def update_user_profile(request, uuid):
+    if request.method == 'PUT':
+        params = QueryDict(request.body)
+        _type = params.get('type')
+        _primary_contact = params.get('primary_contact')
+        _alternate_contact = params.get('alternate_contact')
+        _address = params.get('address')
+        _address_latitude = params.get('address_latitude')
+        _address_longitude = params.get('address_longitude')
+        _landmark = params.get('landmark')
+        _town = params.get('town')
+        _active = params.get('active')
+        user_profile = UserProfile.objects.filter(uuid=uuid, voided=False).first()
+        if not user_profile:
+            response_data = {
+                'error': 'User profile not found',
+            }
+            return JsonResponse(response_data, status=404)
+        user_profile.type = _type
+        user_profile.primary_contact = _primary_contact
+        user_profile.alternate_contact = _alternate_contact
+        user_profile.address = _address
+        user_profile.address_latitude = _address_latitude
+        user_profile.address_longitude = _address_longitude
+        user_profile.landmark = _landmark
+        user_profile.town = _town
+        user_profile.active = _active
+
+        user_profile.save()
+        response_data = {
+            'message': 'User profile updated successfully',
+            'uuid': str(user_profile.uuid),
+        }
+        return JsonResponse({"results": response_data}, status=200)
+    else:
+        return JsonResponse({'error': 'Invalid endpoint or method type'}, status=400)
+
+
+def delete_user_profile(request, uuid):
+    if request.method == 'DELETE':
+        user_profile = UserProfile.objects.filter(uuid=uuid, voided=False).first()
+        if not user_profile:
+            response_data = {
+                'error': 'User profile not found',
+            }
+            return JsonResponse(response_data, status=404)
+        user_profile.delete()
+        response_data = {
+            'message': 'User profile voided successfully'
+        }
+        return JsonResponse(response_data, status=200)
+    else:
+        return JsonResponse({'error': 'Invalid endpoint or method type'}, status=400)
